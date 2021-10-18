989c620f3f939e0bd532d85971e325cee845eabc
lunarbridge <fibremint@gmail.com>
2021-10-18 17:45:11 +0900

    Update version value

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fs2_env',
-    version='0.1.0',
+    version='0.1.1',
     author='Lunar Bridge',
     author_email='horyuurabbit@gmail.com',
     packages=find_packages(),
